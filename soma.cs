f9ff78dd9779295b36ed5dd6897657d7bc912e28
gbdslx <gabiussauro@gmail.com>
2019-10-27 17:19:09 -0200

    exercicio de soma em c#

---
+++ soma.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+            int A, B, X;
+
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+
+            X = A + B;
+
+            Console.WriteLine("X = " + X);
+ 
+        }
+    }
+}
